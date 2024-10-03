85dc97be4df2a959f1e959cacca4eaecd90d05ed
Horváth Kristóf <hk20021084@gmail.com>
2024-10-03 23:07:09 +0200

    Added complex vectors and matrices

---
+++ Scripts/MathData/matrix.gd
@@ -0,0 +1,78 @@
+class_name MatrixComplex2D extends Node
+
+#
+# Properties
+# Using a,b,c,d instead of array for operation cleannes
+#
+
+var a: Complex
+var b: Complex
+var c: Complex
+var d: Complex
+
+#
+# Constructor
+#
+
+func _init(
+	a_value: Complex = Complex.new(),
+	b_value: Complex = Complex.new(),
+	c_value: Complex = Complex.new(),
+	d_value: Complex = Complex.new(),
+) -> void:
+	a = a_value
+	b = b_value
+	c = c_value
+	d = d_value
+
+static func new_real(
+	a_value: float = 0,
+	b_value: float = 0,
+	c_value: float = 0,
+	d_value: float = 0,
+) -> MatrixComplex2D:
+	return MatrixComplex2D.new(
+		Complex.new(a_value),
+		Complex.new(b_value),
+		Complex.new(c_value),
+		Complex.new(d_value),
+	)
+	
+#
+# Operations
+#
+
+## Sums this matrix with `m` and returns the result
+func add(m: MatrixComplex2D) -> MatrixComplex2D:
+	return MatrixComplex2D.new(
+		a.add(m.a), b.add(m.b),
+		c.add(m.c), d.add(m.d),
+	)
+
+## Multiplies by `v` vector from the right side (as a column vector - M * v)
+func multiply_vec(v: VectorComplex2D) -> VectorComplex2D:
+	return VectorComplex2D.new(
+		a.multiply(v.x).add(b.multiply(v.y)), # a * x + b * y
+		c.multiply(v.x).add(d.multiply(v.y)), # c * x + d * y
+	)
+
+## Multiplies by `m` matrix from the right side (M * m)
+func multiply_mat(m: MatrixComplex2D) -> MatrixComplex2D:
+	return MatrixComplex2D.new(
+		a.multiply(m.a).add(b.multiply(m.c)),
+		a.multiply(m.b).add(b.multiply(m.d)),
+		c.multiply(m.a).add(d.multiply(m.c)),
+		c.multiply(m.b).add(d.multiply(m.d)),
+	)
+
+func transpose() -> MatrixComplex2D:
+	return MatrixComplex2D.new(c, a, d, b)
+
+## Replaces the [] operator overload
+func get_value(x: int, y: int) -> Complex:
+	assert(x >= 0 && x < 2 && y >= 0 && y < 2, "Error: Invalid index for MatrixComplex2D")
+	
+	if x == 0:
+		return a if y == 0 else b
+	else:
+		return c if y == 0 else d
---
+++ Scripts/MathData/vector.gd
@@ -0,0 +1,48 @@
+class_name VectorComplex2D extends Object
+## Class to handle 2D vectors containing complex numbers
+
+#
+# Properties
+#
+
+var x: Complex
+var y: Complex
+
+#
+# Constructors
+#
+
+func _init(x_value: Complex = Complex.new(), y_value: Complex = Complex.new()) -> void:
+	x = x_value
+	y = y_value
+
+static func new_real(x_value: float = 0, y_value: float = 1) -> VectorComplex2D:
+	return VectorComplex2D.new(
+		Complex.new(x_value),
+		Complex.new(y_value),
+	)
+
+#
+# Operations
+#
+
+## Returns the sum of this vector with `v`
+func add(v: VectorComplex2D) -> VectorComplex2D:
+	return VectorComplex2D.new(x.add(v.x), y.add(v.y))
+
+## Multiplies by `v` vector from the right side (V * v)
+## *Obviously the result is the same as dot product*
+func multiply_vec(v: VectorComplex2D) -> Complex:
+	return x.multiply(v.x).add(y.multiply(v.y))
+
+## Multiplies by `m` matrix from the right side (V * m as if V was row wise)
+func multiply_mat(m: MatrixComplex2D) -> VectorComplex2D:
+	return VectorComplex2D.new(
+		x.multiply(m.a).add(y.multiply(m.c)),
+		x.multiply(m.b).add(y.multiply(m.d)),
+	)
+
+## Replaces the [] operator overload
+func get_value(i: int) -> Complex:
+	assert(i < 2 && i >= 0, "Error: Invalid index for VectorComplex2D")
+	return x if i == 0 else y
